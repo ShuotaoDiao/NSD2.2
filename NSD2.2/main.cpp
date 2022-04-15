dc08c17604a782323b3bd5bafd3f077a382d357e
Shuotao Diao <sdiao@usc.edu>
2022-04-15 14:44:51 -0700

    Initial Commit

---
+++ NSD2.2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  NSD2.2
+//
+//  Created by Shuotao Diao on 4/15/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
